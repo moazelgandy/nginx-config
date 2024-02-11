42a751ae373aa08f1189b19a950ed9c41af790b7
moazelgandy <77331931+moazelgandy@users.noreply.github.com>
2024-02-11 21:45:06 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -8,6 +8,9 @@ DB_NAME="faca"
 # Backup directory
 BACKUP_DIR="/var/www/"
 
+# Dropbox access token
+DROPBOX_ACCESS_TOKEN="sl.BvZvWTcKklVBzpR1frPKYwp2vJfkJQEvxui7KjkzkZmO6FUEE5sxex4KYG-0PIKIpOlQm2pQ26vJGbXtRvLslMWtzHunnPDM8yJ2MUvtnqFmoD-qlVly-FN2Sc3Zl04nsvSU0s3-F7w5"
+
 while true; do
     # Timestamp
     TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
@@ -24,10 +27,23 @@ while true; do
     # Check if the backup was successful
     if [ $? -eq 0 ]; then
         echo "Database backup created successfully: $BACKUP_FILE" >> /var/www/log_file.log
+        
+        # Upload the backup file to Dropbox
+        curl -X POST \
+             -H "Authorization: Bearer $DROPBOX_ACCESS_TOKEN" \
+             -H "Content-Type: application/octet-stream" \
+             --data-binary @"$BACKUP_FILE" \
+             "https://content.dropboxapi.com/2/files/upload" > /dev/null
+        
+        # Check if the upload was successful
+        if [ $? -eq 0 ]; then
+            echo "Backup uploaded to Dropbox successfully"
+        else
+            echo "Error: Failed to upload backup to Dropbox"
+        fi
+        
     else
         echo "Error: Database backup failed!" >> /var/www/log_file.log
-        # Send email notification
-        mail -s "Database backup failed!" /var/www/log_file.log
     fi
 
     # Sleep for 60 seconds
